a8e7015a2a2e704202e506d63579856217128e55
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-14 21:37:57 +0530

    added php operators

--- index.php
+++ index.php
@@ -1,11 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Project</title>
 </head>
+
 <body>
-    This is php my website. 
+    <div class="">
+        This is php my website. <br>
+
+        <?php
+
+
+
+
+        // Airthmatic operators.
+
+        $a = 23;
+        $b = 67;
+
+        echo "the value of var a + b is : ";
+        echo $a + $b;
+        echo "<br>";
+
+        echo "the value of var a - b is : ";
+        echo $a - $b;
+        echo "<br>";
+
+        echo "the value of var a / b is : ";
+        echo $a / $b;
+        echo "<br>";
+
+        echo "the value of var a * b is : ";
+        echo $a * $b;
+        echo "<br>";
+
+        echo "the value of var a % b is : ";
+        echo $a % $b;
+        echo "<br>";
+
+
+        // Assignment operators.
+        // Comparision operators.
+        // Increment/ Decrement operators.
+        // Logical operators.
+        //  operators.
+
+
+
+        // echo $a - $b;
+        // echo $a / $b;
+        // echo $a * $b;
+
+        ?>
+
+    </div>
 </body>
+
 </html>
--- notes.md
+++ notes.md
@@ -9,9 +9,40 @@ server returns the response.
 
 ### Basics of php.
 
-```
 It is important that our project should be in htdocs folder.
 
+```
 
+to write in php we use this syntax and echo also php is non case sensitive. echo and ECHO both are correct.
 
+<?php
+    echo "Hello world this is printed using php"
+?>
+
+for spacing we use
+
+echo "<br>"
+
+```
+
+### Coding in php
+
+Variables :-
+
+Variables in php are dynamic type means they change their type according to value.
+
+```
+ $var = 23;
+ echo $var ;
+
+ we can declare a variable and use it like this.
+
+```
+
+---
+
+operators in php
+
+```
+echo $var + $kar; // sum of these two variables.
 ```
