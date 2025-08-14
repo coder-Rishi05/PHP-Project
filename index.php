9d1b762987f71b64033b37374e910cfef5bfb2b7
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-14 21:48:44 +0530

    seperate files

--- index.php
+++ index.php
@@ -11,25 +11,8 @@
     <div class="">
         This is php my website. <br>
 
-        <?php
-
-
-        echo "Comparision operators";
-
-
-        // Increment/ Decrement operators.
-        // Logical operators.
-        //  operators.
-
-
-
-        // echo $a - $b;
-        // echo $a / $b;
-        // echo $a * $b;
-
-        ?>
-
     </div>
+    
 </body>
 
 </html>
---
+++ scrip.php
@@ -0,0 +1,17 @@
+
+<?php
+
+echo "Comparision operators";
+
+
+// Increment/ Decrement operators.
+// Logical operators.
+//  operators.
+
+
+
+// echo $a - $b;
+// echo $a / $b;
+// echo $a * $b;
+
+?>
